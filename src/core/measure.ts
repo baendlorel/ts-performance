b28943bf26c92171cb54930cbf1ae355b9671c27
aldia_mechdream <futami16237@gmail.com>
2025-07-02 23:59:36 +0800

    fix: 原来...展开的数组太大会超过最大堆栈； 测试现在可以传入config了； config添加字段array_size，默认为0，如果为0则不会显示出来；

--- src/core/measure.ts
+++ src/core/measure.ts
@@ -5,6 +5,7 @@ import { results } from './result';
 const CONFIG_LABEL = Symbol('config-label');
 type Config = Record<string, number | string> & {
   RUN_TIME: number;
+  ARRAY_SIZE: number;
   [CONFIG_LABEL]: string;
 };
 
@@ -47,7 +48,7 @@ class Measure {
         for (const config of t.configs) {
           const start = performance.now();
           for (let i = 1; i <= config.RUN_TIME; i++) {
-            t.fn();
+            t.fn(config);
           }
           const end = performance.now();
           ReflectDeep.set(results, [t.testName, config[CONFIG_LABEL], t.label], {
@@ -61,7 +62,12 @@ class Measure {
     }
   }
 
-  private addTask(opts: { label: string; extra: boolean; focus: boolean; fn: () => void }) {
+  private addTask(opts: {
+    label: string;
+    extra: boolean;
+    focus: boolean;
+    fn: (config: Config) => void;
+  }) {
     this.tasks.push({
       configs: this.configs,
       fn: opts.fn,
@@ -95,7 +101,7 @@ class Measure {
   }
 
   addConfig(config: Partial<Config>) {
-    const newConfig = Object.assign({ RUN_TIME: 1 }, config) as Config;
+    const newConfig = Object.assign({ RUN_TIME: 1, ARRAY_SIZE: 0 }, config) as Config;
     const label: string[] = [
       `${chalk.blueBright('RUN_TIME')}: ${chalk.cyanBright(formatNum(newConfig.RUN_TIME))}`,
     ];
@@ -103,17 +109,20 @@ class Measure {
       if (k === 'RUN_TIME') {
         continue;
       }
+      if (k === 'ARRAY_SIZE' && newConfig.ARRAY_SIZE === 0) {
+        continue; // skip if ARRAY_SIZE is not set
+      }
       label.push(`${k}: ${chalk.cyanBright(formatNum(newConfig[k]))}`);
     }
     newConfig[CONFIG_LABEL] = label.join(', ');
     this.configs.push(newConfig);
   }
 
-  add(label: string, fn: () => void) {
+  add(label: string, fn: (config: Config) => void) {
     this.addTask({ label, extra: false, focus: false, fn });
   }
 
-  extra(label: string, fn: () => void) {
+  extra(label: string, fn: (config: Config) => void) {
     this.addTask({ label, extra: true, focus: false, fn });
   }
 }
--- src/performance/array-fill.ts
+++ src/performance/array-fill.ts
@@ -1,22 +1,19 @@
 import { measure } from '@/core';
 
 measure.test('Array Fill', () => {
-  const RUN_TIME = 100;
-  const ARRAY_SIZE = 1_000_000; // 需要补多少个 0
+  measure.addConfig({ RUN_TIME: 1000000, ARRAY_SIZE: 10 });
+  measure.addConfig({ RUN_TIME: 1000, ARRAY_SIZE: 10000 });
 
-  measure.addConfig({ RUN_TIME, ARRAY_SIZE });
-
-  measure.add('while-push', () => {
-    let arr1: number[] = [];
-    while (arr1.length < ARRAY_SIZE) {
+  measure.add('while-push', (config) => {
+    const arr1: number[] = [];
+    while (arr1.length < config.ARRAY_SIZE) {
       arr1.push(0);
     }
   });
 
-  measure.add('push(...Array(LENGTH).fill(0))', () => {
-    let arr2: number[] = [];
-    const a = Array(ARRAY_SIZE).fill(0);
-    console.log(a.length);
+  measure.add('push(...Array(LENGTH).fill(0))', (config) => {
+    const arr2: number[] = [];
+    const a = Array(config.ARRAY_SIZE).fill(0);
     arr2.push(...a);
   });
 });
